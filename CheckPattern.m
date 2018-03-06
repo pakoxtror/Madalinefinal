c9d2038e09aa912b8470c84624bd01800b2e7b23
pakoxtror <32783550+pakoxtror@users.noreply.github.com>
2018-03-06 16:32:28 -0500

    Update CheckPattern.m

--- CheckPattern.m
+++ CheckPattern.m
@@ -1,9 +1,10 @@
 function output = CheckPattern (Data,W)
-
 Z=Data(:,end);
 InputP=Data(:,1:end-1);
 Input=[InputP -ones(size(Data,1),1)];
 H=(Input*W);
 Y=sign(H);
-output=isequal(Y,Z);
+Output=mode(Y,2);
+
+output=isequal(Output,Z);
 end
